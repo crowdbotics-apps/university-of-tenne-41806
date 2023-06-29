b851efd65e96aeda94792c6b860eeb858fcc9e13
Crowdbotics <team@crowdbotics.com>
2023-06-29 12:59:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "university-of-tenne-41806",
+  url: "https://university-of-tenne-41806.botics.co",
+  api: "https://university-of-tenne-41806.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
